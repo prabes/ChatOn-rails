89356d42a211058d7ed18eb45a3a47f7e2aabeaa
rdeyvil <rdeyvil11@gmail.com>
2019-09-08 17:14:40 +0545

    Enable flash messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,11 @@
 //= require turbolinks
 //= require semantic-ui
 //= require_tree .
+
+
+$(document).on('turbolinks:load',function){ 
+  $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+      $(this).closest('.message').transition('fade');
+    });
+})  
